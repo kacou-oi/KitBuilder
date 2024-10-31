11c87cdb4af437532dc7b5ba9d6ba9865546f1a6
Ange Kacou Oi <ange.kacou.oi@gmail.com>
2024-10-31 00:51:52 +0000

    Update and rename pro-elements.php to kits-elements.php

--- kits-elements.php
+++ kits-elements.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: PRO Elements
- * Description: This plugin enables GPL features of Elementor Pro: widgets, theme builder, dynamic colors & content, forms & popup builder, and more. Note that PRO Elements is not a substitute for Elementor Pro. If you need all Elementor Pro features, including access to pro templates library and dedicated support, we encourage you to <a href="https://elementor.com/pro/" target="_blank">purchase Elementor Pro</a>.
+ * Plugin Name: Kits Elements
+ * Description: This plugin enables GPL features of Elementor Pro: widgets, theme builder, dynamic colors & content, forms & popup builder, and more. Note that PRO Elements is not a substitute for Elementor Pro.
  * Plugin URI: https://proelements.org/
  * Author: PROElements.org
- * Version: 3.24.4
+ * Version: Beta
  * * Elementor tested up to: 3.23.0
  * Author URI: https://proelements.org/
  * Text Domain: elementor-pro
